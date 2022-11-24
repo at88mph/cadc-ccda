bb574fd829ad20f6546bb29e3ca34754b56f2965
Dustin Jenkins <djenkins.cadc@gmail.com>
2022-11-24 12:52:57 -0800

    Updated tests and removed Google API key.

--- en/megapipe/access/graph.markdown
+++ en/megapipe/access/graph.markdown
@@ -4,23 +4,9 @@ lang: en
 permalink: /en/megapipe/access/graph.html
 title: MegaCam graphical search tool
 altLangPage: /fr/megapipe/access/graph.html
-dateModified: 2021-12-08
-
+dateModified: 2022-11-24
 signing: layout.signing
-
-
 nav: megapipe
-
-# This flag more than one script to be used.
-script:
-    first:
-
-script:
-  - https://maps.googleapis.com/maps/api/js?key=AIzaSyDguHe66O5ep-Lqcla9a44wW_RkhB53KB4&amp;sensor=false
-  - /static/js/megapipe/data.js
-  - /static/js/megapipe/baseutils.js
-  - /static/js/megapipe/cfhtls.js
-  - /static/js/megapipe/keydragzoom.js
 
 css:
   - /static/css/megapipe.css
--- test/test-site.js
+++ test/test-site.js
@@ -1,5 +1,15 @@
 const webdriver = require('selenium-webdriver')
 const seleniumHubServer = 'http://mach378.cadc.dao.nrc.ca:4444/wd/hub'
+const languageConfigurations = [
+    {
+        lang: 'en',
+        title: 'Canadian Astronomy Data Centre'
+    },
+    {
+        lang: 'fr',
+        title: 'Centre canadien de données astronomiques'
+    }
+]
 
 async function runTestWithCaps(capabilities) {
   console.log(`Using Selenium at ${seleniumHubServer}`)
@@ -12,11 +22,9 @@ async function runTestWithCaps(capabilities) {
     .build()
 
     try {
-        console.log('Checking /en/')
-        await driver.get("https://jenkinsd.cadc.dao.nrc.ca/en/")
-
-        console.log('Waiting for title')
-        await driver.wait(webdriver.until.titleContains('Canadian Astronomy Data Centre'), 5000)
+        const target = process.env.TARGET_URL || 'https://www.cadc-ccda.hia-iha.nrc-cnrc-gc.ca'
+        console.log(`Checking consent banner at ${target}/en/`)
+        await driver.get(`${target}/en/`)
 
         console.log('Waiting for consent banner')
         await driver.wait(webdriver.until.elementIsVisible(await driver.findElement(webdriver.By.css('button.popup-modal-dismiss'))), 3000)
@@ -30,36 +38,44 @@ async function runTestWithCaps(capabilities) {
         const englishButton = await driver.findElement(webdriver.By.xpath('//div[contains(@class, "sp-hb")]/div[1]/div[3]/section[1]/p/a'))
         await englishButton.click()
 
-        console.log('Waiting for main container')
-        await driver.wait(webdriver.until.elementIsVisible(await driver.findElement(webdriver.By.css('main.container'))), 3000)
-
-        // Collect the archive links on the main page.
-        const archiveLinks = await driver.findElements(webdriver.By.xpath('//main[contains(@class, "container")]/div[2]/div/dl/dt/a'))
-
-        const archiveLinkCount = archiveLinks.length
-        console.log(`Found ${archiveLinkCount} archive links`)
-
-        const archiveLinkDriver = new webdriver.Builder()
-                                               .usingServer(seleniumHubServer)
-                                               .withCapabilities({
-                                               ...capabilities,
-                                               ...capabilities['browser'] && { browserName: capabilities['browser'] }  // Because NodeJS language binding requires browserName to be defined
-                                               })
-                                               .build()
-
-        for (let i = 0; i < archiveLinkCount; i++) {
-            const elem = archiveLinks[i]
-            await driver.executeScript("arguments[0].scrollIntoView(true);", elem)
-            const href = await elem.getAttribute('href')
-            const linkName = await elem.getText()
-
-            console.log(`Trying ${linkName}`)
-            // Try the link.
-            await archiveLinkDriver.get(href)
-            const title = await archiveLinkDriver.getTitle()
-            if (title.match(/^404.*/)) {
-                throw new Error(`Broken link "${linkName}"`)
-            }
+        for (const langConfig of languageConfigurations) {
+            console.log(`Checking ${target}/${langConfig.lang}/`)
+            await driver.get(`${target}/${langConfig.lang}/`)
+    
+            console.log('Waiting for title')
+            await driver.wait(webdriver.until.titleMatches(new RegExp(`^${langConfig.title}`)), 5000)
+        
+            console.log('Waiting for main container')
+            await driver.wait(webdriver.until.elementIsVisible(await driver.findElement(webdriver.By.css('main.container'))), 3000)
+    
+            // Collect the archive links on the main page.
+            const archiveLinks = await driver.findElements(webdriver.By.xpath('//main[contains(@class, "container")]/div[2]/div/dl/dt/a'))
+    
+            const archiveLinkCount = archiveLinks.length
+            console.log(`Found ${archiveLinkCount} archive links`)
+    
+            const archiveLinkDriver = new webdriver.Builder()
+                                                   .usingServer(seleniumHubServer)
+                                                   .withCapabilities({
+                                                   ...capabilities,
+                                                   ...capabilities['browser'] && { browserName: capabilities['browser'] }  // Because NodeJS language binding requires browserName to be defined
+                                                   })
+                                                   .build()
+    
+            for (let i = 0; i < archiveLinkCount; i++) {
+                const elem = archiveLinks[i]
+                await driver.executeScript("arguments[0].scrollIntoView(true);", elem)
+                const href = await elem.getAttribute('href')
+                const linkName = await elem.getText()
+    
+                console.log(`Trying ${linkName}`)
+                // Try the link.
+                await archiveLinkDriver.get(href)
+                const title = await archiveLinkDriver.getTitle()
+                if (title.match(/^404.*/)) {
+                    throw new Error(`Broken link "${linkName}"`)
+                }
+            } 
         }
     } finally {
         console.log('Quitting!')
